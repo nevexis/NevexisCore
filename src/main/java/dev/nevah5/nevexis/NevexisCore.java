6777ee69f38dc3041037daf154fb0cf919673e83
Nevah5 <neevaah5@gmail.com>
2023-09-24 23:05:43 +0200

    Initial Commit

---
+++ src/main/java/dev/nevah5/nevexis/NevexisCore.java
@@ -0,0 +1,15 @@
+package dev.nevah5.nevexis;
+
+import dev.nevah5.nevexis.command.VanishCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class NevexisCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Executes when plugin enables
+    }
+
+}
