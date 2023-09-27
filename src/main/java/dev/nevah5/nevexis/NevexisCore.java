df2ce6b034c9f8d0ad12ea48d23b3623e3b16fb2
Nevah5 <neevaah5@gmail.com>
2023-09-27 18:46:30 +0200

    #7 Added custom join and quit formats

--- src/main/java/dev/nevah5/nevexis/NevexisCore.java
+++ src/main/java/dev/nevah5/nevexis/NevexisCore.java
@@ -2,6 +2,8 @@ package dev.nevah5.nevexis;
 
 import dev.nevah5.nevexis.command.VanishCommand;
 import dev.nevah5.nevexis.listener.ChatListener;
+import dev.nevah5.nevexis.listener.JoinListener;
+import dev.nevah5.nevexis.listener.QuitListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -14,6 +16,8 @@ public final class NevexisCore extends JavaPlugin {
 
         // Listeners
         this.getServer().getPluginManager().registerEvents(new ChatListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new QuitListener(this), this);
 
         // Commands
         Objects.requireNonNull(this.getCommand("vanish")).setExecutor(new VanishCommand(this));
