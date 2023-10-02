3c3e97d740011d743cdffdade9e1b58b0475e92c
Nevah5 <neevaah5@gmail.com>
2023-10-02 22:57:42 +0200

    #15 Don't send blocked commands to player

--- src/main/java/dev/nevah5/nevexis/NevexisCore.java
+++ src/main/java/dev/nevah5/nevexis/NevexisCore.java
@@ -1,10 +1,7 @@
 package dev.nevah5.nevexis;
 
 import dev.nevah5.nevexis.command.VanishCommand;
-import dev.nevah5.nevexis.listener.ChatListener;
-import dev.nevah5.nevexis.listener.CommandListener;
-import dev.nevah5.nevexis.listener.JoinListener;
-import dev.nevah5.nevexis.listener.QuitListener;
+import dev.nevah5.nevexis.listener.*;
 import dev.nevah5.nevexis.webhook.DiscordWebhook;
 import dev.nevah5.nevexis.webhook.DiscordWebhookUtil;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,6 +19,7 @@ public final class NevexisCore extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new JoinListener(this), this);
         this.getServer().getPluginManager().registerEvents(new QuitListener(this), this);
         this.getServer().getPluginManager().registerEvents(new CommandListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new CommandSendListener(this), this);
 
         // Commands
         Objects.requireNonNull(this.getCommand("vanish")).setExecutor(new VanishCommand(this));
