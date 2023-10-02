c5a0f97056cb97d0a8ef1f6710995866d53b7025
Nevah5 <neevaah5@gmail.com>
2023-10-02 18:02:55 +0200

    #13 Altered Webhook Styles

--- src/main/java/dev/nevah5/nevexis/webhook/DiscordWebhookUtil.java
+++ src/main/java/dev/nevah5/nevexis/webhook/DiscordWebhookUtil.java
@@ -10,6 +10,7 @@ public class DiscordWebhookUtil {
         return new DiscordWebhook().builder()
                 .setEmbedTitle("Plugin State")
                 .setEmbedDescription(isEnabled ? "Plugin is now enabled." : "Plugin is now disabled.")
+                .setEmbedTimestamp()
                 .setEmbedColor(isEnabled ? 6750105 : 11739419)
                 .build();
     }
@@ -17,7 +18,9 @@ public class DiscordWebhookUtil {
     public static DiscordWebhook joinActivity(final PlayerJoinEvent joinEvent) {
         return new DiscordWebhook().builder()
                 .setEmbedTitle("Join Activity")
+                .setEmbedAuthor(joinEvent.getPlayer().getUniqueId().toString(), null, null)
                 .setEmbedDescription(joinEvent.getPlayer().getName() + " joined.")
+                .setEmbedTimestamp()
                 .setEmbedColor(6750105)
                 .build();
     }
@@ -25,7 +28,9 @@ public class DiscordWebhookUtil {
     public static DiscordWebhook quitActivity(final PlayerQuitEvent quitEvent) {
         return new DiscordWebhook().builder()
                 .setEmbedTitle("Quit Activity")
+                .setEmbedAuthor(quitEvent.getPlayer().getUniqueId().toString(), null, null)
                 .setEmbedDescription(quitEvent.getPlayer().getName() + " left.")
+                .setEmbedTimestamp()
                 .setEmbedColor(11739419)
                 .build();
     }
@@ -33,7 +38,9 @@ public class DiscordWebhookUtil {
     public static DiscordWebhook chatActivity(final AsyncPlayerChatEvent chatEvent) {
         return new DiscordWebhook().builder()
                 .setEmbedTitle("Chat Activity")
-                .setEmbedDescription(chatEvent.getPlayer().getName() + " >> " + chatEvent.getMessage())
+                .setEmbedAuthor(chatEvent.getPlayer().getUniqueId().toString(), null, null)
+                .addEmbedField(chatEvent.getPlayer().getName(), chatEvent.getMessage(), false)
+                .setEmbedTimestamp()
                 .setEmbedColor(16777215)
                 .build();
     }
