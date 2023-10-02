ab4576f1afa4140111df9d84393fccff4ecdebc4
Nevah5 <neevaah5@gmail.com>
2023-10-02 23:21:45 +0200

    #19 Fixed

--- src/main/java/dev/nevah5/nevexis/NevexisCore.java
+++ src/main/java/dev/nevah5/nevexis/NevexisCore.java
@@ -4,15 +4,21 @@ import dev.nevah5.nevexis.command.VanishCommand;
 import dev.nevah5.nevexis.listener.*;
 import dev.nevah5.nevexis.webhook.DiscordWebhook;
 import dev.nevah5.nevexis.webhook.DiscordWebhookUtil;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public final class NevexisCore extends JavaPlugin {
 
+    public String NO_PERMISSION;
+
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+
+        // Configurations
+        this.NO_PERMISSION = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("core.no-permission"));
 
         // Listeners
         this.getServer().getPluginManager().registerEvents(new ChatListener(this), this);
